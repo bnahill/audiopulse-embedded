522aaf90de97c1704841ff08ca0e7c65ee034929
Ben Nahill <bnahill@gmail.com>
2014-02-03 21:55:48 -0500

    Small fixes to UI

--- python_ui/apulse_ui.py
+++ python_ui/apulse_ui.py
@@ -7,8 +7,7 @@ import time
 
 import matplotlib
 matplotlib.use('Qt4Agg')
-matplotlib.rcParams['backend.qt4']='PySide'
-import pylab
+matplotlib.rcParams['backend.qt4'] = 'PySide'
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
