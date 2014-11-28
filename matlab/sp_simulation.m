d0739a71ac56d252ec6f9e77018102c97ed96866
Ikaro Silva <ikarosilva@yahoo.com>
2014-11-27 22:45:08 -0500

    added noise and stimulus respones to the analysis

--- matlab/sp_simulation.m
+++ matlab/sp_simulation.m
@@ -1,21 +1,22 @@
 clear all;clc;close
 
-%Define simulation parameters
 
+%Define simulation parameters
 Fs=48000;  %Original sampling frequency
 T=8;       %Recording time in seconds
 F1=2000;   %First stimulus
 F2=2400;   %Second stimulus
+F3=2*F1-F2; %Expected Response
 A=65;      %Stimulus amplitude in dBu
-L=512;%384;%Fs*T/1000;   %Averaging windowLength L <= Fs*T, total windows ~ Fs*T/L
+L=512;     %Averaging windowLength L <= Fs*T, total windows ~ Fs*T/L
 step=L/2;  %Amount of over in samples, (step=L/2 -> 50%, step=L -> 0%)
-aproxFreq=true;  %If this is set to true, will approximate F1 and F2 such that FFT leakage due to choice of L is minimized (see below).
+aproxFreq=false;  %If this is set to true, will approximate F1 and F2 such that FFT leakage due to choice of L is minimized (see below).
+noisedB=60; 
+%End of simulation parameters %
 
 %Decimation parameters
 decFs=16000;      %Final frequency
 b=fir1(28, 6/24); % Filter parameters for low pass to be applied before decimation
-
-%End of simulation parameters %
 
 %Note: In order to avoid leakage due to the number of samples in the FFT,
 %the frequency should be a multiple of 2*pi/L (See See Hayes 1999 pg 239
@@ -24,27 +25,29 @@ if(aproxFreq)
      Fstep=Fs/L;
      F1=round(F1/Fstep)*Fstep;
      F2=round(F2/Fstep)*Fstep;
+     F3=2*F1-F2;
 end
 
 
 tm=[0:1/Fs:T];
+N=length(tm);
 tone1=sin(2*pi*F1*tm);
 tone2=sin(2*pi*F2*tm);
+tone3=sin(2*pi*F3*tm);
+noise=randn(1,N);
 RMS_TONE=sqrt(2);
+RMS_NOISE=sqrt(mean(noise.^2));
 
-
-%Define windows and get its RMS
-win=rectwin(L);%nuttallwin(L);
-POW_WIN=mean(abs(win).^2);
-
-
-%Normalize gain by the RMS of TONE and WINDOW
+%Normalization factor
 gain=RMS_TONE*10^(A/20);
+ngain=RMS_NOISE.*10^(noisedB/20);
 
 %Normalize the tones and generate the stimulus
 tone1=tone1.*gain;
 tone2=tone2.*gain;
-x=tone1+tone2;
+tone3=tone3.*gain;
+noise=noise.*ngain;
+x= tone1 + tone2 + tone3 + noise;
 
 
 %Filter and decimate the signal
@@ -53,15 +56,28 @@ if(~isempty(decFs))
     x=filter(b,1,x);
     x=x(1:K:end);
     Fs=decFs;
-    tm=[0:1/Fs:T]; 
+    tm=[0:1/Fs:T];
+    N=length(tm);
 end
 
 
-N=length(x);
 M=floor(N/step);
 if(M*step > L)
     M=M-1;
 end
+
+
+%Define window types  to compare
+WIN={'rectwin','blackman','hamming','nuttallwin'};
+clr=['brkg'];
+NWIN=length(WIN);
+
+for nwin=1:NWIN
+
+
+%Define windows and get its RMS
+eval(['win=' WIN{nwin} '(L);'])
+POW_WIN=mean(abs(win).^2);
 
 %Get running average of FFTs (we are estimating via WELCH's method)
 X=zeros(1,L);
@@ -78,16 +94,17 @@ freq = 0:Fs/length(X):Fs;
 freq(end)=[];
 
 %Get stats
-maxX=max(X);
+[maxX,indMax]=max(X);
 RMS_X=20*log10(sqrt(mean(x.^2)));
 
 %Plot everything
-plot(freq,X)
+plot(freq,X,clr(nwin)); hold on
 grid on
-title(['Fmax= ' num2str(maxX) ' dB, Amp= ' num2str(A) ' dB, x RMS (dB) = ' num2str(RMS_X)])
+title(['Fmax= ' num2str(maxX) ' dB, Amp= ' num2str(A) ' dB, x RMS (dB) = ' num2str(RMS_X) ' , Fmax= ' num2str(freq(indMax))])
 xlabel('Frequency (Hz)')
 ylabel('Power/Frequency (dB/Hz)')
 xlim([0 4000])
 
-
+end
+legend(WIN(:))
 
